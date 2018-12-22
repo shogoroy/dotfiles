44a26f04a85a47a3400e8981baa47566723c3a9f
Shogoro Yoshida <shogyosh@yahoo-corp.jp>
2018-12-22 10:50:56 +0900

    :heavy_plus_sign: feat(zsh): add history setting

---
+++ zsh/history.zsh
@@ -0,0 +1,23 @@
+# 履歴ファイルの保存先
+export HISTFILE=${HOME}/.zsh_history
+
+# メモリに保存される履歴の件数
+export HISTSIZE=1000
+
+# 履歴ファイルに保存される履歴の件数
+export SAVEHIST=100000
+
+# 重複を記録しない
+setopt hist_ignore_all_dups
+
+# 余分な空白は詰めて記録
+setopt hist_reduce_blanks
+
+# historyコマンドは履歴に登録しない
+setopt hist_no_store
+
+# 補完時にヒストリを自動的に展開
+setopt hist_expand
+
+# 開始と終了を記録
+setopt EXTENDED_HISTORY
