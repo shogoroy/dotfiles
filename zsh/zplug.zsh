f2f24999ad5d1ab5fbc0b76d8db42e4ecf1b224f
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2019-07-26 14:27:06 +0900

    :sparkles: :wrench: Add zplug settings

---
+++ zsh/zplug.zsh
@@ -0,0 +1,18 @@
+# Install zplug if not
+if [ ! -e ~/.zplug ]; then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
+
+source ~/.zplug/init.zsh
+
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+  zplug install
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load --verbose
