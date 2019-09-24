51b576301d376c98cec76414d9629059b863b0b6
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2019-09-25 02:37:18 +0900

    :heavy_plus_sign: Add efm-langserver via zplug

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -9,6 +9,8 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
+zplug "mattn/efm-langserver", from:gh-r, as:command
+
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
   zplug install
