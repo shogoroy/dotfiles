ac72de272ee09b75eca20b32536e83836a1fa3d9
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2020-03-08 21:22:11 +0900

    :wrench: Update plugins via zplug

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -5,11 +5,16 @@ fi
 
 source ~/.zplug/init.zsh
 
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 zplug "mattn/efm-langserver", from:gh-r, as:command
+zplug "x-motemen/ghq", from:gh-r, as:command, rename-to:ghq
+zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf
+zplug "junegunn/fzf", as:command, use:bin/fzf-tmux
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
